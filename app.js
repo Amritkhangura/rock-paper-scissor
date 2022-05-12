14e13c988f943f69374f72f4f6e5cfb8d90181e0
Amrit Khangura <amritkhangura65@gmail.com>
2022-05-11 18:39:34 -0700

    basic Javascript done.

--- app.js
+++ app.js
@@ -1,50 +1,72 @@
-function userPlay(){
-  let random = ["rock", "paper", "scissors"];
-  return random[Math.floor(Math.random() * 3)];
-}
-function computerPlay() {
-  let random = ["rock", "paper", "scissors"];
-  return random[Math.floor(Math.random() * 3)];
-}
-userPlay();
-computerPlay();
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".results > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
 
-function playAround(playerSelection, computerSelection) { //plays a round of the game.
-        if (playerSelection === "rock") {
-            if (computerSelection === "rock") {
-                return "Draw!";
-            } else if (computerSelection === "paper") {
-            return "Computer wins!";
-            } else {
-                return "User wins!";
-            }
-        } else if (playerSelection === "paper") {
-            if (computerSelection === "rock") {
-                return "User wins!";
-            } else if (computerSelection === "paper") {
-                return "Draw!";
-            } else {
-                return "Computer wins!";
-            }
-        } else {
-            if (computerSelection === "rock") {
-                return "Computer wins!";
-            } else if (computerSelection === "paper") {
-                return "User wins!";
-            } else {
-                return "Draw!";
-            }
-        }
-    }
-    for (let i = 0; i < 5; i++) {
-console.log("points are", playAround(i));
+function getComputerChoice() {
+  const choices = ['r', 'p', 's'];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+function convert(letter){
+  if (letter === "r") return "Rock";
+  if (letter === "p") return "Paper";
+  return "Scissors"
+}
+function win(user, comp){
+ userScore++;
+ userScore_span.innerHTML = userScore;
+ result_div.innerHTML = `${convert(user)} beats ${convert(comp)}. You win.`;
+}
+function lose(user, comp){
+  computerScore++;
+  computerScore_span.innerHTML = computerScore;
+  result_div.innerHTML = `${convert(comp)} beats ${convert(user)}. You lose.`;
+
+}
+function draw(){
+  console.log("Draw");
+  result_div.innerHTML = `Draw`;
 }
 
-const playerSelection = "paper";
-console.log(playAround(playerSelection, computerSelection));
-var userChoice = userPlay();
-var computerSelection = computerPlay();
-var result = playAround(userChoice, computerSelection)
-console.log("user's choice", userChoice);
-console.log("computer's choice", computerSelection);
-console.log("Result is", result);
+function game(userChoice) {
+const computerChoice = getComputerChoice();
+switch (userChoice + computerChoice) {
+  case "rs":
+  case "pr":
+  case "sp":
+    win(userChoice, computerChoice);
+    break;
+  case "rp":
+  case "ps":
+  case "sr":
+    lose(userChoice, computerChoice);
+    break;
+  case "rr":
+  case "pp":
+  case "ss":
+    draw(userChoice, computerChoice);
+  break;
+}
+}
+game();
+
+function main() {
+rock_div.addEventListener('click', function(){
+  game("r");
+});
+paper_div.addEventListener('click', function(){
+  game("p");
+});
+scissors_div.addEventListener('click', function(){
+  game("s");
+});
+}
+
+main();
+getComputerChoice();
